3a52f3243a43e6c7f3204613e134518849090490
Nurfadhilah <nurfadhilahalang480@gmail.com>
2022-01-23 23:07:48 +0800

    refactor again

--- MathBasic/src/MathBasic.js
+++ MathBasic/src/MathBasic.js
@@ -4,8 +4,7 @@ const MathBasic = {
       throw new Error('fungsi add hanya menerima dua parameter');
     }
 
-    const a = args[0];
-    const b = args[1];
+    const [a, b] = args;
 
     if (typeof a !== 'number' || typeof b !== 'number') {
       throw new Error('fungsi hanya menerima parameter number');
