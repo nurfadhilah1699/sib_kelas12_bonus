f4b0ccb34df8c53b4c00abf0962f8d0779c501c7
Nurfadhilah <nurfadhilahalang480@gmail.com>
2022-01-23 22:19:22 +0800

    write test case in MathBasic.test.js

---
+++ MathBasic/src/MathBasic.test.js
@@ -0,0 +1,14 @@
+const MathBasic = require('./MathBasic');
+
+describe('A MathBasic', () => {
+  it('should contains add, subtract, multiply, and divide function', () => {
+    expect(MathBasic).toHaveProperty('add');
+    expect(MathBasic).toHaveProperty('subtract');
+    expect(MathBasic).toHaveProperty('multiply');
+    expect(MathBasic).toHaveProperty('divide');
+    expect(MathBasic.add).toBeInstanceOf(Function);
+    expect(MathBasic.subtract).toBeInstanceOf(Function);
+    expect(MathBasic.multiply).toBeInstanceOf(Function);
+    expect(MathBasic.divide).toBeInstanceOf(Function);
+  });
+});
