0103f222555daedd17a630fb6b345e0d79f24be1
Nurfadhilah <nurfadhilahalang480@gmail.com>
2022-01-23 22:57:40 +0800

    doing 2nd refactor

--- MathBasic/src/MathBasic.js
+++ MathBasic/src/MathBasic.js
@@ -1,6 +1,6 @@
 const MathBasic = {
   add: (...args) => {
-    if (args.length < 2 || args.length > 2) {
+    if (args.length !== 2) {
       throw new Error('fungsi add hanya menerima dua parameter');
     }
   },
